bbe05b5940e927710bb184b92f57b35cd5e21bba
EltonTahiri <eltontahiri4@gmail.com>
2022-06-16 21:12:55 +0200

    Number changes color when it goes up and down

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 let count = 0;
-let colors = ["red" , "green"]
+let number = document.querySelector("#number");
 
 document.getElementById('decrease').onclick = function (){
     count--;
     document.getElementById('number').innerHTML = count
+    number.style.color = "red"
 }
 
 document.getElementById('increase').onclick = function () {
     count++;
     document.getElementById('number').innerHTML = count;
+    number.style.color = "green"
 }
 
 document.getElementById('reset').onclick = function() {
     count = 0;
-    document.getElementById('number').innerHTML = count
+    document.getElementById('number').innerHTML = count 
 }
 
 
